f8551b487a0d16b98debd398627ae1dc8eb4cdde
nekobanana <pru98@hotmail.it>
2025-02-18 22:14:00 +0100

    feat: better random choice of transforms

--- sequential.py
+++ sequential.py
@@ -8,6 +8,14 @@ def augment_images(input_dir, output_dir, num_augmented=5):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
+    transformations = [
+        A.RandomBrightnessContrast(brightness_limit=(0.2, 0.8), contrast_limit=(0.2, 0.8), p=1),
+        A.GaussNoise(std_range=(0.2, 0.8), mean_range=(-0.8, 0.8), p=1),
+        A.RandomGamma(gamma_limit=(80, 120), p=1),
+        A.Blur(blur_limit=(3, 5), p=1),
+        A.CLAHE(clip_limit=(1, 4), p=1)
+    ]
+
     for filename in os.listdir(input_dir):
         input_path = os.path.join(input_dir, filename)
         if not os.path.isfile(input_path):
@@ -20,21 +28,12 @@ def augment_images(input_dir, output_dir, num_augmented=5):
 
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         for i in range(num_augmented):
-            transform = A.Compose([
-                A.RandomBrightnessContrast(brightness_limit=(0.2, 0.8), contrast_limit=(0.2, 0.8), p=get_rand()),
-                A.GaussNoise(std_range=(0.2, 0.8), mean_range=(-0.8, 0.8), p=get_rand()),
-                A.RandomGamma(gamma_limit=(80, 120), p=get_rand()),
-                A.Blur(blur_limit=(3, 5), p=get_rand()),
-                A.CLAHE(clip_limit=(1, 4), p=get_rand()),
-            ])
-
-            augmented = transform(image=image)['image']
+            transform_n = random.randint(0, len(transformations))
+            transform_list = random.sample(transformations, transform_n)
+            transform_compose = A.Compose(transform_list)
+            augmented = transform_compose(image=image)['image']
             output_path = os.path.join(output_dir, f"{os.path.splitext(filename)[0]}_aug_{i}.jpg")
             cv2.imwrite(output_path, cv2.cvtColor(augmented, cv2.COLOR_RGB2BGR))
-
-
-def get_rand():
-    return random.uniform(0, 1)
 
 
 if __name__ == '__main__':
