0cf9d2d5907822f7c9ba44dd5795767acbe02720
nekobanana <pru98@hotmail.it>
2025-02-20 23:58:21 +0100

    fix: script fix

--- script.sh
+++ script.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 python=".venv/bin/python"
-for p in $(seq 4 4 32);
+base_dir="/media/quacksort/OS/Users/giorg/parallel"
+for p in $(seq 8 8 32);
 do
-  $python parallel_transf.py -i input_images -o output_images -a 100 -p "$p" -r "results/parallel_trans_{$p}.txt"
-  $python parallel_images.py -i input_images -o output_images -a 100 -p "$p" -r "results/parallel_images_{$p}.txt"
+  $python parallel_transf.py -i input_images -o "$base_dir/output_images" -a 10 -p "$p" -r "results/parallel_trans_$p.txt"
+  $python parallel_images.py -i input_images -o "$base_dir/output_images" -a 10 -p "$p" -r "results/parallel_images_$p.txt"
 done
-$python sequential.py -i input_images -o output_images -a 100 -r "results/parallel_images_{$p}.txt"
+$python sequential.py -i input_images -o "$base_dir/output_images" -a 10 -r "results/parallel_images_$p.txt"
